d6540c7c4bb41ee89e88bf1a127b70d9b42f8bb2
Ashwini Bokade <aashu.24win@gmail.com>
2021-09-24 15:20:42 +0530

    Message

---
+++ ChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatApp.Hubs
+{
+    public class ChatHub:Hub
+    {
+        public async Task SendMessage(string userId, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", userId, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Clients.All.SendAsync("ReceiveMessage", "system", $"{Context.ConnectionId} joined the conversation");
+            return base.OnConnectedAsync();
+        }
+    }
+}
